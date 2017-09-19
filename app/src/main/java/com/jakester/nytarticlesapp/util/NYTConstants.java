0f562417281502e68ca1cc6a26ef6a9197ecda56
Jake Rushing <jakester0812@gmail.com>
2017-09-18 23:02:46 -0700

    First commit, added README and added API Key

---
+++ app/src/main/java/com/jakester/nytarticlesapp/util/NYTConstants.java
@@ -0,0 +1,10 @@
+package com.jakester.nytarticlesapp.util;
+
+/**
+ * Created by Jake on 9/18/2017.
+ */
+
+public class NYTConstants {
+
+    public static final String ARTICLES_SEARCH_API_KEY = "b142e76bf1f24179af9d13cbf2403258";
+}
